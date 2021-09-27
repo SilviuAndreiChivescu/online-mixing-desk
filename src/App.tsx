4b90678376a571d9d002a547d282c8fbed8b6582
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2021-09-27 20:09:03 +0100

    Makes UI better

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import "./App.css";
 import { useAudio } from "./AppLogic";
 import { Oscilloscope } from "./components/Oscilloscope";
@@ -19,12 +19,32 @@ const App: React.FC = () => {
     draw,
   } = useAudio("/assets/outfoxing.mp3");
 
+  const [compressor, setCompressor] = useState(false);
+  const [filter, setFilter] = useState(false);
+
   return (
     <Container fluid className="App text-center">
       <p>This is the start of an awesome project</p>
 
       <Oscilloscope draw={draw} />
-      <section style={{ width: "30%", margin: "0 auto" }}>
+      <Container>
+        <Row>
+          <Col>
+            <Button className="mt-5" onClick={play}>
+              Play
+            </Button>
+          </Col>
+          <Col>
+            <Button className=" mt-5" onClick={pause}>
+              Pause
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <section
+        style={{ width: "30%", margin: "0 auto" }}
+        className="text-start"
+      >
         <MyRangeSlider
           onChangeFunction={volumeControl}
           label="Volume"
@@ -42,15 +62,32 @@ const App: React.FC = () => {
           step={1}
           className="mt-4 mb-4"
         />
+        <Row>
+          <Col>
+            <Form.Check
+              name="compressor"
+              type="checkbox"
+              label="Compressor"
+              aria-label="Compressor"
+              onChange={() => setCompressor((currValue) => !currValue)}
+            />
+          </Col>
+          <Col>
+            <Form.Check
+              name="filter"
+              type="checkbox"
+              label="Filter"
+              aria-label="Filter"
+              onChange={() => setFilter((currValue) => !currValue)}
+            />
+          </Col>
+        </Row>
 
-        <Compressor compressorControl={compressorControl} />
+        {compressor ? (
+          <Compressor compressorControl={compressorControl} />
+        ) : null}
 
-        <Filter biquadFilterControl={biquadFilterControl} />
-
-        <Button onClick={play}>Play</Button>
-        <Button className="ms-5" onClick={pause}>
-          Pause
-        </Button>
+        {filter ? <Filter biquadFilterControl={biquadFilterControl} /> : null}
       </section>
     </Container>
   );
