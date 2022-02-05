b2fccc160a6f783cd5e3ba3981d46ed723151cf4
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2022-02-05 18:19:57 +0000

    Changes to reflect new code

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,78 @@ import { Filter } from "./components/Filter";
 
 import Reverb from "./components/Reverb";
 
-const App: React.FC = () => {
-  const {
-    play,
-    pause,
-    gainControl,
-    pannerControl,
-    compressorControl,
-    biquadFilterControl,
-    draw,
-    chooseImpulse,
-  } = useAudio("/assets/outfoxing.mp3");
+// v2
+import { useInit } from "./hooks/useInit";
 
+// const App: React.FC = () => {
+//   const {
+//     play,
+//     pause,
+//     gainControl,
+//     pannerControl,
+//     compressorControl,
+//     biquadFilterControl,
+//     draw,
+//     chooseImpulse,
+//   } = useAudio("/assets/outfoxing.mp3");
+
+//   return (
+//     <Container fluid className="App text-center">
+//       <Oscilloscope draw={draw} />
+//       <Container>
+//         <Row>
+//           <Col>
+//             <Button className="mt-5" onClick={play}>
+//               Play
+//             </Button>
+//           </Col>
+//           <Col>
+//             <Button className=" mt-5" onClick={pause}>
+//               Pause
+//             </Button>
+//           </Col>
+//         </Row>
+//       </Container>
+//       <section
+//         style={{ width: "30%", margin: "0 auto" }}
+//         className="text-start"
+//       >
+//         {/* Reverb */}
+//         {/* <Reverb chooseImpulse={chooseImpulse} /> */}
+
+//         {/* Gain and Panning */}
+//         {/* <MyRangeSlider
+//           onChangeFunction={(e) => gainControl(e)}
+//           label="Gain"
+//           min={0}
+//           max={2}
+//           defaultValue={1}
+//           step={0.1}
+//         />
+//         <MyRangeSlider
+//           onChangeFunction={(e) => pannerControl(e)}
+//           label="Pan"
+//           min={-1}
+//           max={1}
+//           defaultValue={0}
+//           step={0.01}
+//           className="mt-4 mb-4"
+//         /> */}
+//         {/* END Gain and Panning */}
+
+//         {/* Commented below to test FX unit to finish 'internal chain design' */}
+//         {/* <Compressor compressorControl={compressorControl} />
+//         <Filter biquadFilterControl={biquadFilterControl} /> */}
+//       </section>
+//     </Container>
+//   );
+// };
+
+const App: React.FC = () => {
+  const [channelOneFunctions] = useInit();
+  const { play, pause } = channelOneFunctions;
   return (
-    <Container fluid className="App text-center">
-      <Oscilloscope draw={draw} />
+    <div className="App">
       <Container>
         <Row>
           <Col>
@@ -39,38 +96,7 @@ const App: React.FC = () => {
           </Col>
         </Row>
       </Container>
-      <section
-        style={{ width: "30%", margin: "0 auto" }}
-        className="text-start"
-      >
-        {/* Reverb */}
-        {/* <Reverb chooseImpulse={chooseImpulse} /> */}
-
-        {/* Gain and Panning */}
-        {/* <MyRangeSlider
-          onChangeFunction={(e) => gainControl(e)}
-          label="Gain"
-          min={0}
-          max={2}
-          defaultValue={1}
-          step={0.1}
-        />
-        <MyRangeSlider
-          onChangeFunction={(e) => pannerControl(e)}
-          label="Pan"
-          min={-1}
-          max={1}
-          defaultValue={0}
-          step={0.01}
-          className="mt-4 mb-4"
-        /> */}
-        {/* END Gain and Panning */}
-
-        {/* Commented below to test FX unit to finish 'internal chain design' */}
-        {/* <Compressor compressorControl={compressorControl} />
-        <Filter biquadFilterControl={biquadFilterControl} /> */}
-      </section>
-    </Container>
+    </div>
   );
 };
 
