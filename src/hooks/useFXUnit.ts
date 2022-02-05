5b4d0ac35aef9a5d22a3759e9277d65d7603d2e2
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2022-02-05 10:15:46 +0000

    Finishes FX Unit hook

---
+++ src/hooks/useFXUnit.ts
@@ -0,0 +1,60 @@
+import { useReverb } from "./useReverb";
+import { useGain } from "./useGain";
+import { useEffect, useState } from "react";
+
+const useFXUnit = (audioCtx: AudioContext, compressorOutput: GainNode) => {
+  const [FXUnitOutput] = useGain(audioCtx);
+
+  const [convolver, chooseImpulse] = useReverb(audioCtx);
+
+  const [dryGainNode, dryGainControl] = useGain(audioCtx);
+  const [wetGainNode, wetGainControl] = useGain(audioCtx);
+
+  // Dry Wet Mix Controller Knob (100 means full wet)
+  const [dryWetKnob, setDryWetKnob] = useState(0.5);
+  useEffect(() => {
+    if (dryWetKnob > 0.5) {
+      dryGainControl(1 - 2 * (dryWetKnob - 0.5));
+      wetGainControl(1);
+    } else {
+      wetGainControl(1 - 2 * (0.5 - dryWetKnob));
+      dryGainControl(1);
+    }
+  }, [dryWetKnob]);
+
+  // Connect reverb to wetGainNode and to FXUnit output
+  convolver.connect(wetGainNode).connect(FXUnitOutput);
+
+  // Connect dryGainNode to FXUnit output
+  dryGainNode.connect(FXUnitOutput);
+
+  // Connect FX Unit
+  const connectFXUnit = () => {
+    compressorOutput.disconnect();
+
+    compressorOutput.connect(convolver);
+    compressorOutput.connect(dryGainNode);
+  };
+
+  // Disconnect FX Unit
+  const disconnectFXUnit = () => {
+    compressorOutput.disconnect();
+
+    compressorOutput.connect(FXUnitOutput);
+  };
+
+  // Put everything to export into an object
+  const [FXUnitFunctions] = useState({
+    chooseImpulse: chooseImpulse,
+    connectFXUnit: connectFXUnit,
+    disconnectFXUnit: disconnectFXUnit,
+    FXUnitOutput: FXUnitOutput,
+    FXUnitDryWet: {
+      dryWetKnob: dryWetKnob,
+      setDryWetKnob: setDryWetKnob,
+    },
+  });
+
+  return [FXUnitFunctions] as const;
+};
+export { useFXUnit };
