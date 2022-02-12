0120c8bbd7d77c7ddc0cccc1c2fc5fb030f7646d
Silviu-Andrei Chivescu <s.a.chivescu@gmail.com>
2022-02-12 14:29:01 +0000

    Updates onOffButton component

--- src/components/OnOffButton.tsx
+++ src/components/OnOffButton.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 
 interface OnOffButtonProps {
   id: string;
   onChange: () => void;
+  checkedArray?: any[];
 }
 
-function OnOffButton({ id, onChange }: OnOffButtonProps) {
+function OnOffButton({ id, onChange, checkedArray }: OnOffButtonProps) {
   return (
     <Row>
-      <Col className="mt-2 mb-2">
-        <ToggleButtonGroup type="checkbox" onChange={onChange}>
-          <ToggleButton variant="outline-dark" id={id} value="1">
-            IN
+      <Col className="mb-2">
+        <ToggleButtonGroup
+          type="checkbox"
+          value={checkedArray}
+          onChange={onChange}
+        >
+          <ToggleButton variant="outline-dark" size="sm" id={id} value={1}>
+            In
           </ToggleButton>
         </ToggleButtonGroup>
       </Col>
